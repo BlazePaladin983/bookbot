17b220b5fc4da558a2b0106861ef932baf91cdb5
BlakeBepler <Blake2194438@gmail.com>
2025-09-16 00:57:06 -0700

    update python to print entire book

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    path = f"{filepath}"
+    with open(path) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    path_file = get_book_text("books/frankenstein.txt")
+    return print(path_file)
+
+main()
