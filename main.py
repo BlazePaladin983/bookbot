25821051e34625d1145853b58f1a7e8213924f90
BlakeBepler <Blake2194438@gmail.com>
2025-09-18 11:31:59 -0700

    tells word count of frankenstein.txt

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def get_book_text(filepath):
     path = f"{filepath}"
-    with open(path) as f:
+    with open(path, encoding="utf-8") as f:
         file_contents = f.read()
         return file_contents
 
+def word_count(book):
+    words = len(book.split())
+    return words
+
 def main():
     path_file = get_book_text("books/frankenstein.txt")
-    return print(path_file)
+    num_words = word_count(path_file)
+    return print(f"{num_words} words found in the document")
 
 main()
